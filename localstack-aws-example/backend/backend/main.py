a231a2d85228d9afd1c53932fa8890d06d24eaca
Emad Dehnavi <dehnavi@gmx.com>
2024-11-23 20:05:15 +0100

    upload to s3

--- localstack-aws-example/backend/backend/main.py
+++ localstack-aws-example/backend/backend/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import boto3
+from werkzeug.utils import secure_filename
 
-endpoint_url = "http://localhost.localstack.cloud:4566"
+endpoint_url = "http://localhost:4566/"
 
 app = Flask(__name__)
 
@@ -8,6 +10,25 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify(message="Welcome to our API home!")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    try:
+        if request.method == "POST":
+            print("request.files", request.files)
+            if "MyFile" not in request.files:
+                return jsonify(message="No file part"), 400
+            file = request.files["MyFile"]
+            filename = secure_filename(file.filename)
+            client = boto3.client("s3", endpoint_url=endpoint_url)
+            client.put_object(
+                Bucket="localstack-demo", Key="uploads/" + filename, Body=file
+            )
+    except:
+        print("Something went wrong")
+    else:
+        return jsonify(message="File uploaded successfully to S3 bucket"), 200
 
 
 def start():
